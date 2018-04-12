d437d7a116f0a638132b6cbd620b19c36a467f2d
DESKTOP-B3EKVPO\YEOBI <wts1597@gmail.com>
2018-04-12 15:25:19 +0900

    [Added] Classes (1F~4F)

---
+++ Animulator/Assets/PlayerBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    private Transform _transform;
+    private Rigidbody _rigidbody;
+
+    private Vector3 _lookPoint;
+
+	// Use this for initialization
+	void Start () {
+        _transform = GetComponent<Transform>();
+        _rigidbody = GetComponent<Rigidbody>();
+
+        _lookPoint = Vector3.back;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _rigidbody.velocity = _transform.localRotation * new Vector3(Input.GetAxis("Horizontal") * 4f, _rigidbody.velocity.y, Input.GetAxis("Vertical") * 4f);
+        _lookPoint = Vector3.Lerp(_lookPoint, _rigidbody.velocity, Time.deltaTime * 5f);
+        _transform.LookAt(_transform.position + _lookPoint);
+	}
+}
