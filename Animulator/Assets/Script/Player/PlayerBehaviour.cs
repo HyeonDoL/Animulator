dcf09ed785d3bb96c9727d1e1bf2daf8da147feb
LAPTOP-87CAPFA5\godd7 <godd7776@gmail.com>
2018-04-13 12:25:30 +0900

    Add PlayerMove, PlayerJump

---
+++ Animulator/Assets/Script/Player/PlayerBehaviour.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// FIXME : 추후 상태가 추가될 경우, 상태들을 객체로 분리하여 FSM 구현하기
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    [SerializeField]
+    private float jumpHeight = 1f;
+
+    [SerializeField]
+    private Rigidbody rigidbody;
+
+    private float horizontal;
+    private float vertical;
+
+    public bool IsGround { get; set; }
+    public bool IsFalling { get; set; }
+
+    private void Awake()
+    {
+        IsGround = true;
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGround)
+            StartCoroutine(Jump());
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+
+        if (IsFalling)
+            CheckGrounded();
+    }
+
+    public void Move()
+    {
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * moveSpeed * Time.deltaTime;
+
+        transform.Translate(movement, Space.Self);
+    }
+
+    public IEnumerator Jump()
+    {
+        rigidbody.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
+
+
+        IsGround = false;
+
+        yield return new WaitForSeconds(0.5f);
+
+        IsFalling = true;
+
+    }
+
+    public void CheckGrounded()
+    {
+        float distanceToGround;
+        float threshold = 1.2f;
+        RaycastHit hit;
+
+        if (Physics.Raycast((rigidbody.position), -Vector3.up, out hit, 100f))
+        {
+            distanceToGround = hit.distance;
+
+            Debug.Log(hit.distance);
+
+            if (distanceToGround < threshold)
+            {
+                IsGround = true;
+                IsFalling = false;
+            }
+        }
+    }
+}
---
+++ Animulator/Assets/Script/Player/PlayerDirection.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerDirection : MonoBehaviour
+{
+    private const float maxRotationY = 45.0f;
+    private const float minRotationY = -45.0f;
+
+    private float sensitivityX = 1.5f;
+    private float sensitivityY = 1.5f;
+
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
+
+    private Transform cameraTrans;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+
+        cameraTrans = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationY = Mathf.Clamp(rotationY, minRotationY, maxRotationY);
+
+        cameraTrans.rotation = Quaternion.Euler(-rotationY, rotationX, 0);
+        transform.rotation = Quaternion.Euler(0, rotationX, 0);
+    }
+}
